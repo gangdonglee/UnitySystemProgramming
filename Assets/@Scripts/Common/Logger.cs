09532011bef792d6c4898b1de7d1b248e49a2313
gangdonglee <69913785+gangdonglee@users.noreply.github.com>
2024-07-17 23:03:53 +0900

    스크립트 추가
    로거 싱글톤 추가

--- Assets/@Scripts/Common/Logger.cs
+++ Assets/@Scripts/Common/Logger.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 // 1. 추가적인 정보 표현 (ex. 타임스탬프)
-public class Logger : MonoBehaviour
+// 2. 출시용 빌드를 위한 로그 제거
+public static class Logger 
 {
-    void Start()
+    [Conditional("DEV_VER")]
+    public static void Log(string msg)
     {
-        
+        UnityEngine.Debug.LogFormat("{0} {1}", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), msg);
     }
-
-    void Update()
+    [Conditional("DEV_VER")]
+    public static void LogWarning(string msg)
     {
-        
+        UnityEngine.Debug.LogFormat("{0} {1}", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), msg);
+    }
+    [Conditional("DEV_VER")]
+    public static void LogError(string msg)
+    {
+        UnityEngine.Debug.LogFormat("{0} {1}", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), msg);
     }
 }
---
+++ Assets/@Scripts/Common/SingletonBehaviour.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
+{
+    // 씬 전환 시 삭제할지 여부
+    protected bool m_IsDestoryOnLoad = false;
+
+    //이 클래스의 Static Instance 변수
+    protected static T m_Instance;
+
+    public static T Instacne
+    {
+        get { return m_Instance; }
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+    protected virtual void Init()
+    {
+        if (m_Instance == null)
+        {
+            m_Instance = (T)this;
+
+            if (!m_IsDestoryOnLoad) { DontDestroyOnLoad(this); }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // 삭제 시 실행되는 함수
+    protected virtual void OnDestory()
+    {
+        Dispose();
+    }
+    // 삭제 시 추가로 처리해 주어야할 작업을 여기서 처리
+    protected virtual void Dispose()
+    {
+        m_Instance = null;
+    }
+}

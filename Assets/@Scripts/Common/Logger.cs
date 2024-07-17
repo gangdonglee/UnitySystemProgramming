f9980a2fd2e4e5899565d8b7fe13fbdf79c309c6
gangdonglee <69913785+gangdonglee@users.noreply.github.com>
2024-07-17 22:42:48 +0900

    Logger 추가

---
+++ Assets/@Scripts/Common/Logger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1. 추가적인 정보 표현 (ex. 타임스탬프)
+public class Logger : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
